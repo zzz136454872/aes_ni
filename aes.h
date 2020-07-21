a8f6ef69815f4507eb543a594128cdd5ce96ec88
f4prime <zzz136454872@163.com>
2020-07-21 16:15:44 +0800

    update aes.h

--- aes.h
+++ aes.h
@@ -1,10 +1,11 @@
+// 
 #ifndef aes_h
+
 #include<immintrin.h>
 
-void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char* subkey);
-void aes_128_dec(unsigned char *cipher,unsigned char *plain,unsigned char* subkey);
-void aes_128_key_expansion(unsigned char* 
+void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,(unsigned char* subkey)[128]);
+void aes_128_dec(unsigned char *cipher,unsigned char *plain,(unsigned char*subkey[128]);
+void aes_128_key_expansion(unsigned char* key,(unsigned char* subkey)[128]);
 
 #endif
-
 
