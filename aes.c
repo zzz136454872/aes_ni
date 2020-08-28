6d4ae70a74de957beda8c1f85135dbdbf40de964
f4prime <zzz136454872@163.com>
2020-08-28 20:02:06 +0800

    add key expansion and enc

--- aes.c
+++ aes.c
@@ -1,26 +1,125 @@
 #include"aes.h"
 
-unsigned char rcon[14]={0x00/* a padding */,
+const unsigned char rcon[14]={0x00/* a padding */,
     0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1b,0x36};
 //TODO add 10~13 for aes 192 and 256
 
-void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16]);
+void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16])
+{
+    __m128i process=_mm_loadu_si128((__m128i*)plain);
+    __m128i round_key=_mm_loadu_si128((__m128i*)subkey);
+    process=_mm_xor_si128(process,round_key);
+    int i;
+    for(i=1;i<10;i++)
+    {
+        round_key=_mm_loadu_si128((__m128i*)subkey+i);
+        process=_mm_aesenc_si128(process,round_key);
+    }
+    round_key=_mm_loadu_si128((__m128i*)subkey+10);
+    process=_mm_aesenclast_si128(process,round_key);
+    _mm_storeu_si128((__m128*)cipher,process);
+}
 
-void aes_128_dec(unsigned char *cipher,unsigned char *plain,unsigned char (*subkey)[16]);
+void aes_128_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char (*subkey)[16]);
 
 void aes_128_key_expansion(unsigned char* key,unsigned char (* subkey)[16])
 {
-    int i=0;
-    unsigned int tmp_int;
-    int *pi=(int*)(subkey+1);
+    unsigned int buffer[4];
+    unsigned int *pi=(unsigned int*)subkey;
+    unsigned char tmp_char;
     memcpy(subkey[0],key,16);
     __m128i tmp;
-    for(i=4;i<44;i++)
-    {
-        if(i%4!=0)
-            pi[i]=pi[i-1]^pi[i-4];
-    }
+    int i=4;
+    //round1
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x01);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round2
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x02);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round3
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x04);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round4
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x08);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round5
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x10);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round6
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x20);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round7
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x40);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round8
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x80);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round9
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x1b);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    //round10
+    tmp=_mm_set1_epi32(pi[i-1]);
+    tmp_char=rcon[i/4];
+    tmp=_mm_aeskeygenassist_si128(tmp,0x36);
+    _mm_storeu_si128((__m128i*)buffer,tmp);
+    pi[i++]=pi[i-4]^buffer[1];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
+    pi[i++]=pi[i-1]^pi[i-4];
 }
-
 
 
--- test.c
+++ test.c
@@ -40,10 +40,11 @@ long long nstimer()
 }
 
 // a test key
-unsigned char key[16]={0x01,0x02,};
+unsigned char key[16]={0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c};
 // the buffer for round key
 unsigned char subkey[15][16];
-unsigned char plain[16];
+unsigned char plain[16]={0x32,0x43,0xf6,0xa8,0x88,0x5a,0x30,0x8d,0x31,0x31,0x98,0xa2,0xe0,0x37,0x07,0x34};
+
 unsigned char cipher[16];
 unsigned char decrypt[16];
 
@@ -52,6 +53,9 @@ int main()
     printmem(key,16,"key");
     aes_128_key_expansion(key,subkey);
     printmem(subkey,240,"subkey128");
+    aes_128_enc_block(plain,cipher,subkey);
+    printmem(plain,16,"plain");
+    printmem(cipher,16,"cipher");
     return 0;
 }
 
