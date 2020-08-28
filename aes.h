cf120e80b20e53caa41b163319960fe5da56f7f2
f4prime <zzz136454872@163.com>
2020-08-28 20:00:23 +0800

    fix function defination

--- aes.h
+++ aes.h
@@ -2,11 +2,12 @@
 #ifndef aes_h
 
 #include<wmmintrin.h>
+#include<emmintrin.h>
 #include<string.h>
 
 void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16]);
 
-void aes_128_dec(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16]);
+void aes_128_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16]);
 
 void aes_128_key_expansion(unsigned char* key,unsigned char(* subkey)[16]);
 
