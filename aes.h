07886cdd20f8d543051440c53b8d04f6ea5e4158
f4prime <zzz136454872@163.com>
2020-08-29 19:58:01 +0800

    add functions

--- aes.h
+++ aes.h
@@ -5,11 +5,25 @@
 #include<emmintrin.h>
 #include<string.h>
 
+//bits -> nk
+//128  -> 4
+//192  -> 6
+//256  -> 8
+void aes_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16],unsigned char nk);
+void aes_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16],unsigned char nk);
+void aes_key_expansion(unsigned char* key,unsigned char(* subkey)[16],unsigned char nk);
+
+void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16]);
+void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16]);
 void aes_128_enc_block(unsigned char *plain,unsigned char* cipher,unsigned char (*subkey)[16]);
 
 void aes_128_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16]);
+void aes_128_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16]);
+void aes_128_dec_block(unsigned char *cipher,unsigned char *plain,unsigned char(*subkey)[16]);
 
 void aes_128_key_expansion(unsigned char* key,unsigned char(* subkey)[16]);
+void aes_192_key_expansion(unsigned char* key,unsigned char(* subkey)[16]);
+void aes_256_key_expansion(unsigned char* key,unsigned char(* subkey)[16]);
 
 #endif
 
